d8315e2a1a5881831381e4766cbee4f3b1b3eb56
rabiademirbag <rddemirbagg@gmail.com>
2025-05-31 15:35:39 +0300

    Proje dosyası ekle.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool isValid = false;
+        while (!isValid) {
+
+            try
+            {
+                Console.WriteLine("Lütfen bir sayı girin: ");
+                int number=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayının karesi: "+number*number);
+                isValid = true;
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Geçersiz giriş!");
+            }
+            
+        }
+    }
+}
